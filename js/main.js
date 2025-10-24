df53c0c998d54a77fce9547258d32b42fea08b10
Jenny Liu <jianyi2130@gmail.com>
2025-10-24 04:50:30 -0700

    final update for lab2

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ let icon = document.getElementsByClassName("icon")[0];
 // Add an event listener for the 'click' event on the icon element
 icon.addEventListener('click', responsive_control);
 
+function myFunction(x) {
+  x.classList.toggle("fa-thumbs-down");
+}
+
 // Function to control the responsiveness of the navigation bar
 function responsive_control() {
   // Get the element with the id "myTopnav"
@@ -18,3 +22,34 @@ function responsive_control() {
     x.className = "topnav";
   }
 }
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
